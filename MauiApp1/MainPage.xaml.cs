7f69d7fec17cbe91501b2e111bf6097e19e2550a
codegiss <gisellesouza243@gmail.com>
2024-10-05 16:29:54 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Login(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(EntryID.Text) || string.IsNullOrWhiteSpace(EntryPassword.Text))
+            {
+                await DisplayAlert("Aviso", "Insira o login e a senha", "OK");
+                return;
+            }                
+
+            if (EntryID.Text == "admin" && EntryPassword.Text == "senha@dmin")
+                await DisplayAlert("Sucesso", "Login realizado com sucesso", "OK");
+            else
+                await DisplayAlert("Falha", "Login não autorizado", "OK");
+        }
+        private void Clean(object sender, EventArgs e)
+        {
+            EntryID.Text = "";
+            EntryPassword.Text = "";
+            EntryID.Focus();
+        }
+        private async void Credits(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Giselle Souza - CB3020339 \n");
+            sb.Append("Lucas Gomes - CB3021777");
+
+            await DisplayAlert("Créditos", sb.ToString(), "OK");
+        }
+    }
+
+}
